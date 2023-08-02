f299ab1db05d57d38dd1e04f6db6a7092ec695d8
Paulo Gomes <pcesarpgomes@gmail.com>
2023-08-02 16:30:07 -0300

    Notícias com scroll infinito via plugin

--- lib/views/screens/mysql_news.dart
+++ lib/views/screens/mysql_news.dart
@@ -1,5 +1,7 @@
+import 'package:escola_verde_mobile/models/post_model.dart';
 import 'package:escola_verde_mobile/views/widgets/event_button.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import '../widgets/custom_appbar.dart';
 import '../widgets/drawer_appbar.dart';
 
@@ -14,6 +16,79 @@ class MySQLNews extends StatefulWidget {
 }
 
 class _MySQLNewsState extends State<MySQLNews> {
+  static const _pageSize = 10;
+
+  final PagingController<int, PostModel> _pagingController =
+      PagingController(firstPageKey: 0);
+
+  @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+    super.initState();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      posts = await PostData.getPosts();
+      final isLastPage = posts.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(posts);
+      } else {
+        final nextPageKey = pageKey + posts.length;
+        _pagingController.appendPage(posts, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const CustomAppBar('Notícias do Site'),
+      drawer: const DrawerAppbar(),
+      body: PagedListView<int, PostModel>(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<PostModel>(
+            itemBuilder: (context, item, index) {
+          return EventButton(
+              titulo: posts[index].title!,
+              imagePath: '',
+              onTap: () {},
+              date: posts[index].date!);
+        }),
+      ),
+    );
+  }
+
+  /* @override
+  Widget build(BuildContext context) => PagedListView<int, PostModel>(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<PostModel>(
+            itemBuilder: (context, item, index) {
+          return EventButton(
+              titulo: posts[index].title!,
+              imagePath: '',
+              onTap: () {},
+              date: posts[index].date!);
+        }),
+      ); */
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+}
+
+
+// Implementação antiga abaixo (backup temporário)
+/* 
+
+class _MySQLNewsState extends State<MySQLNews> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +97,7 @@ class _MySQLNewsState extends State<MySQLNews> {
       drawer: const DrawerAppbar(),
       body: posts.isNotEmpty
           ? ListView.builder(
-              itemCount: 10,
+              itemCount: 16,
               itemBuilder: (context, index) {
                 return EventButton(
                     titulo: posts[index].title!,
@@ -71,3 +146,5 @@ class _MySQLNewsState extends State<MySQLNews> {
     );
   }
 }
+
+*/
